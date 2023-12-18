4b5bc0697dc4d4472c058acd20ac0e68448fcb0d
ahmad344646 <149039506+ahmad344646@users.noreply.github.com>
2023-12-18 09:54:32 +0500

    Update main.py

--- main.py
+++ main.py
@@ -205,19 +205,12 @@ def running():
         if int(rating) < 11:
             WebDriverWait(driver1, 70).until(
                         EC.element_to_be_clickable((By.XPATH, '//*[@id="maincolumn"]/div/div[1]/div[2]/a[4]'))).click()
-            page_source = driver1.page_source
-            soup = BeautifulSoup(page_source, 'html.parser')
-            
-            b_tags = soup.find_all('b')
-
-# Print the text content of each <b> tag
-            for b_tag in b_tags:
-                print(b_tag.text)
-
-                
-            last_b_tag = soup.find_all('b')[-1] if soup.find_all('b') else None
-            print(f"today total views: {last_b_tag.text}")
-            if int(last_b_tag.text) > 100:
+            dvfdfg = WebDriverWait(driver1, 70).until(
+                        EC.element_to_be_clickable((By.XPATH, ' //*[@id="maincolumn"]/div/div[1]/center/span[1]'))).text
+            number_match = re.search(r'\d+', dvfdfg)
+            extracted_number = int(number_match.group())
+            print(f"today total views: {extracted_number}")
+            if int(extracted_number) > 100:
                 WebDriverWait(driver1, 70).until(
                         EC.element_to_be_clickable((By.XPATH, '//*[@id="maincolumn"]/div/div[1]/center/span[2]'))).click()
                 time.sleep(1)
